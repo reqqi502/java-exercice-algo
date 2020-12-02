982ff34d06233e5a6018ce7e774d9b2b0c6e2f36
reqqi502 <akram.rq.12@gmail.com>
2020-12-02 11:26:47 +0100

    exercice

---
+++ src/pr_pack/verifylogin.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package pr_pack;
+
+/**
+ * @author Admin
+ *
+ */
+public class verifylogin {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String email = "akram.rq.12@gmail.com";
+		String usernam = "akram";
+		String password = "0000";
+		
+
+	}
+	public static void verifylogin(String email ,String usernam , String password) {
+		boolean found = false;
+		String tempUsername = "";
+		String tempPassword = "";
+		
+	}
+
+}
